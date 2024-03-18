c221115d615ebe41e04cfc38341e9a995c32646f
XerShade <xershade.ca@gmail.com>
2024-03-18 05:58:01 -0400

    Removed default authentication system.

--- source/Asreyion/Program.cs
+++ source/Asreyion/Program.cs
@@ -1,12 +1,7 @@
 using Asreyion.Core.Areas.Test.Controllers;
-using Asreyion.Data;
-using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.AspNetCore.Mvc.Routing;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Mvc.Infrastructure;
-using Microsoft.AspNetCore.Mvc.Razor;
-using Microsoft.Extensions.FileProviders;
 
 namespace Asreyion;
 
@@ -17,17 +12,8 @@ public class Program
         WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
         // Add services to the container.
-        string connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-        _ = builder.Services.AddDbContext<ApplicationDbContext>(options =>
-            options.UseSqlServer(connectionString));
-        _ = builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-        _ = builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-            .AddEntityFrameworkStores<ApplicationDbContext>();
-
         _ = builder.Services.AddControllersWithViews()
             .AddApplicationPart(typeof(FunctionController).Assembly);
-
 
         _ = builder.Services.AddSingleton<IActionContextAccessor, ActionContextAccessor>();
         _ = builder.Services.AddScoped<IUrlHelper>(x => {
@@ -39,11 +25,7 @@ public class Program
         WebApplication app = builder.Build();
 
         // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            _ = app.UseMigrationsEndPoint();
-        }
-        else
+        if (!app.Environment.IsDevelopment())
         {
             _ = app.UseExceptionHandler("/Home/Error");
             // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
@@ -55,7 +37,6 @@ public class Program
 
         _ = app.UseRouting();
 
-        _ = app.UseAuthentication();
         _ = app.UseAuthorization();
 
         _ = app.MapControllerRoute(
